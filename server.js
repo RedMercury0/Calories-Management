3c70165d9f384f0e1a2374729414f2ff52e18fa5
RedMecrucy0 <doront2010@gmail.com>
2024-03-21 20:41:25 +0200

    .

--- server.js
+++ server.js
@@ -6,10 +6,20 @@ import mainRoutes from './routes/mainRoutes.js';
 
 const app = express();
 
-// Middlewares
+// Middleware to enable CORS
 app.use(cors());
-app.use(express.json()) //allow the server to accept json in the body of the request
 
+// Middleware to parse JSON in the request body
+app.use(express.json());
+
+// Middleware to handle JSON syntax errors
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        res.status(400).json({ error: 'Invalid JSON syntax' });
+    } else {
+        next();
+    }
+});
 
 // Initial routes specification
 // Main routes
